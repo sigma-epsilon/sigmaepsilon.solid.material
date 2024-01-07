8c2cc40a4ceb2f43066ca8822a5ed1fd84920f1f
Bence Balogh <benceeok@gmail.com>
2024-01-07 10:52:36 +0100

    checking imports

---
+++ src/sigmaepsilon/solid/material/config.py
@@ -0,0 +1,13 @@
+
+try:  # pragma: no cover
+    import cupy
+    __has_cupy__ = True
+except Exception:
+    __has_cupy__ = True
+    
+
+try:  # pragma: no cover
+    from numba import cuda
+    __has_numba_cuda__ = True
+except Exception:
+    __has_numba_cuda__ = True
