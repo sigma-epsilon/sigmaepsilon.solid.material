5af65279114914a74aecf4648c76a3c9d386bb9a
Bence Balogh <benceeok@gmail.com>
2024-01-07 13:34:37 +0100

    set up metadata

--- src/sigmaepsilon/solid/material/__init__.py
+++ src/sigmaepsilon/solid/material/__init__.py
@@ -1,3 +1,8 @@
+from os.path import dirname, abspath
+from importlib.metadata import metadata
+
+from sigmaepsilon.core.config import namespace_package_name
+
 from .frame import BernoulliFrameSection
 from .surface import (
     MindlinShellSection,
@@ -41,3 +46,9 @@ __all__ = [
     "StiffnessLike",
     "FailureLike",
 ]
+
+__pkg_name__ = namespace_package_name(dirname(abspath(__file__)), 10)
+__pkg_metadata__ = metadata(__pkg_name__)
+__version__ = __pkg_metadata__["version"]
+__description__ = __pkg_metadata__["summary"]
+del __pkg_metadata__
